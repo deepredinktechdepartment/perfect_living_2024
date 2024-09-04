2a2a326c65939fcb3813dc090f383c5527c40580
deepredinktechdepartment <109503655+deepredinktechdepartment@users.noreply.github.com>
2024-09-04 16:45:37 +0530

    Update theme_options_list.blade.php

--- resources/views/themeoptions/theme_options_list.blade.php
+++ resources/views/themeoptions/theme_options_list.blade.php
@@ -1,74 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <!-- Start col -->
-    <div class="col-lg-12">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="card">
-                    @if(isset($theme_options_data->id))
-                    <div class="d-flex justify-content-end align-items-center gap-3 mb-2">
-                        <a href="{{ url('theme_options/edit/'.Crypt::encrypt($theme_options_data->id) ?? 0) }}" class="edit mr-2" title="Edit">
-                            {!! Config::get('constants.EditIcon') !!}
-                        </a>
-                    </div>
-
-                    <!-- Header Logo -->
-                    <div class="row align-items-center mb-3">
-                        <div class="col-lg-4">
-                            <h4>Header Logo</h4>
+<div class="container">
+    <div class="row">
+        <!-- Start col -->
+        <div class="col-lg-12">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card shadow-sm">
+                        @if(isset($theme_options_data->id))
+                        <div class="card-header d-flex justify-content-end align-items-center">
+                            <a href="{{ url('theme_options/edit/'.Crypt::encrypt($theme_options_data->id) ?? 0) }}" class="btn btn-link" title="Edit">
+                                {!! Config::get('constants.EditIcon') !!}
+                            </a>
                         </div>
-                        <div class="col-lg-8">
-                            <div>
+
+
+                        <div class="card-body">
+                            <!-- Header Logo -->
+                            <div class="mb-4">
+                                <h5 class="mb-2">Header Logo</h5>
                                 @if(isset($theme_options_data->header_logo) && file_exists(storage_path('app/uploads/' . basename($theme_options_data->header_logo))))
-                                    <img src="{{ URL::to($theme_options_data->header_logo) }}" width="100" />
+                                    <img src="{{ URL::to($theme_options_data->header_logo) }}" class="img-fluid" alt="Header Logo" style="max-width: 100px;">
                                 @else
                                     <p>No header logo available.</p>
                                 @endif
                             </div>
-                        </div>
-                    </div>
 
-                    <!-- Favicon -->
-                    <div class="row align-items-center mb-3">
-                        <div class="col-lg-4">
-                            <h4>Favicon</h4>
-                        </div>
-                        <div class="col-lg-8">
-                            <div>
+                            <!-- Favicon -->
+                            <div class="mb-4">
+                                <h5 class="mb-2">Favicon</h5>
                                 @if(isset($theme_options_data->favicon) && file_exists(storage_path('app/uploads/' . basename($theme_options_data->favicon))))
-                                    <img src="{{ URL::to($theme_options_data->favicon) }}" />
+                                    <img src="{{ URL::to($theme_options_data->favicon) }}" class="img-fluid" alt="Favicon" style="max-width: 50px;">
                                 @else
                                     <p>No favicon available.</p>
                                 @endif
                             </div>
-                        </div>
-                    </div>
 
-                    <!-- Footer Logo -->
-                    <div class="row align-items-center mb-3">
-                        <div class="col-lg-4">
-                            <h4>Footer Logo</h4>
-                        </div>
-                        <div class="col-lg-8">
-                            <div>
+                            <!-- Footer Logo -->
+                            <div class="mb-4">
+                                <h5 class="mb-2">Footer Logo</h5>
                                 @if(isset($theme_options_data->footer_logo) && file_exists(storage_path('app/uploads/' . basename($theme_options_data->footer_logo))))
-                                    <img src="{{ URL::to($theme_options_data->footer_logo) }}" width="50" />
+                                    <img src="{{ URL::to($theme_options_data->footer_logo) }}" class="img-fluid" alt="Footer Logo" style="max-width: 100px;">
                                 @else
                                     <p>No footer logo available.</p>
                                 @endif
                             </div>
-                        </div>
-                    </div>
 
-                    <!-- Copyright -->
-                    <div class="row align-items-center mb-3">
-                        <div class="col-lg-4">
-                            <h4>Copyright</h4>
-                        </div>
-                        <div class="col-lg-8">
-                            <div>
+                            <!-- Copyright -->
+                            <div class="mb-4">
+                                <h5 class="mb-2">Copyright</h5>
                                 @if(isset($theme_options_data->copyright) && !empty($theme_options_data->copyright))
                                     <p>{{ $theme_options_data->copyright }}</p>
                                 @else
@@ -76,12 +57,34 @@
                                 @endif
                             </div>
                         </div>
+                        @endif
                     </div>
-                    @endif
                 </div>
             </div>
         </div>
+        <!-- End col -->
     </div>
-    <!-- End col -->
 </div>
 @endsection
+
+@push('styles')
+<style>
+    .card-title {
+        font-size: 1.25rem;
+        font-weight: 500;
+    }
+
+    .card-header {
+        background-color: #f8f9fa;
+    }
+
+    .card-body img {
+        border: 1px solid #ddd;
+        border-radius: 0.375rem;
+    }
+
+    .mb-4 {
+        margin-bottom: 1.5rem;
+    }
+</style>
+@endpush
