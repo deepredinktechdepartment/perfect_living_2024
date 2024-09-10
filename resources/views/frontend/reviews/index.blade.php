63f3f0e19888306782d1de56ff5c244c91b2e1d0
deepredinktechdepartment <109503655+deepredinktechdepartment@users.noreply.github.com>
2024-09-10 13:29:14 +0530

    update revew page

--- resources/views/frontend/reviews/index.blade.php
+++ resources/views/frontend/reviews/index.blade.php
@@ -9,11 +9,12 @@
 
         <!-- Approved Reviews -->
         @if($approvedReviews->count())
-            <div class="approved-reviews">
+            <div class="approved-reviews row">
 
                 @foreach($approvedReviews as $review)
-                    <div class="review-card mb-3">
-                        <div class="review-rating">
+                <div class="col-sm-3 mb-3">
+                    <div class="review-card card p-4 pb-2">
+                        <div class="review-rating mb-2">
                             @for($i = 1; $i <= 5; $i++)
                                 <i class="fa{{ $i <= $review->star_rating ? 's' : 'r' }} fa-star"></i>
                             @endfor
@@ -23,6 +24,7 @@
                         <p class="review-text">{{ $review->review }}</p>
                         <p class="review-date">{{ $review->created_at->format('M d, Y') }}</p>
                     </div>
+                </div>
                 @endforeach
             </div>
         @else
