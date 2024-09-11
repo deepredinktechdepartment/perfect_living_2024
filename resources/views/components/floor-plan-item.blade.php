6c5d3daf02ee07f9046aaaea46204cdb2aaf2658
deepredinktechdepartment <109503655+deepredinktechdepartment@users.noreply.github.com>
2024-09-11 13:17:53 +0530

    Update floor-plan-item.blade.php

--- resources/views/components/floor-plan-item.blade.php
+++ resources/views/components/floor-plan-item.blade.php
@@ -6,11 +6,11 @@
     <div class="floorplans-images-wrapper">
         <div class="floorplans-image">
         <img src="{{ $image_path }}" alt="{{ $description }}" class="img-fluid">
-        <p class="mb-0 zoom-text">Click to Zoom &nbsp; <i class="fa-solid fa-magnifying-glass-plus"></i></p>  
+        <p class="mb-0 zoom-text">Click to Zoom &nbsp; <i class="fa-solid fa-magnifying-glass-plus"></i></p>
     </div>
         <div class="floorplans-info-text">
-        <h6 class="mb-0">{{ $description }}</h6>
-            <h6>{{ $size }} Sq.ft</h6>
+        <h6 class="mb-0">{{ $description }} {{ $size }} Sq.ft</h6>
+
         </div>
     </div>
 </a>
