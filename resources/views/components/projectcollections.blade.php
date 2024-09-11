fa46749df67e16f4905cb3d8403d84e61a1278c3
deepredinktechdepartment <109503655+deepredinktechdepartment@users.noreply.github.com>
2024-09-11 11:35:54 +0530

    update

--- resources/views/components/projectcollections.blade.php
+++ resources/views/components/projectcollections.blade.php
@@ -2,9 +2,10 @@
 
 <div class="highlights-images-item col-sm-4 p-2">
     <div class="highlights-images-wrapper">
-        <a href="{{ $link ?? '#' }}" target="_new">
+        <a href="{{ $link ?? '#' }}" target="_new" class="text-decoration-none">
             <img src="{{ URL::to($imageSrc) }}" alt="{{ $altText }}" class="img-fluid">
             <div class="highlights-images-text">
+                <p class="hover-text text-white text-end"><i class="fa-solid fa-arrow-up-right-from-square"></i></p>
                 <p class="text-white mb-0">{!! $description !!}</p>
             </div>
         </a>
