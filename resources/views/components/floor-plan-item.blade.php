50b7ba4bb76f39a306a3d92f760115d126b4a9ba
deepredinktechdepartment <109503655+deepredinktechdepartment@users.noreply.github.com>
2024-09-11 10:58:38 +0530

    floorplans update

--- resources/views/components/floor-plan-item.blade.php
+++ resources/views/components/floor-plan-item.blade.php
@@ -4,9 +4,12 @@
 <div class="floorplans-item col-sm-4 p-2">
 <a href="{{ $image_path }}" data-fancybox="floorplan_images" data-caption="{{ $description }} {{ $size }} Sq.ft">
     <div class="floorplans-images-wrapper">
+        <div class="floorplans-image">
         <img src="{{ $image_path }}" alt="{{ $description }}" class="img-fluid">
+        <p class="mb-0 zoom-text">Click to Zoom</p>  
+    </div>
         <div class="floorplans-info-text">
-            <h6 class="mb-0">{{ $description }}</h6>
+        <h6 class="mb-0">{{ $description }}</h6>
             <h6>{{ $size }} Sq.ft</h6>
         </div>
     </div>
