10683c5c51516b94150b8f072cfd0fc8c2ac5027
deepredinktechdepartment <109503655+deepredinktechdepartment@users.noreply.github.com>
2024-09-12 14:54:29 +0530

    Update main.blade.php

--- resources/views/layouts/frontend_theme/main.blade.php
+++ resources/views/layouts/frontend_theme/main.blade.php
@@ -8,6 +8,7 @@
     @if(isset($favicon) && File::exists($favicon))
 <link rel="shortcut icon" href="{{ URL::to(asset($favicon)) }}">
 @else
+@endif
 
     <!-- Include CSS files here -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
