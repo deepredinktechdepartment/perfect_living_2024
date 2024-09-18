72b69fab383bf4ce8f98945cc1a6df73ac720b6a
Suneel Garnepudi <61911808+Suneel-rgb@users.noreply.github.com>
2024-09-18 17:06:12 +0530

    banner images updated

--- resources/views/frontend/pages/fitlersprojects.blade.php
+++ resources/views/frontend/pages/fitlersprojects.blade.php
@@ -3,16 +3,27 @@
 @extends('layouts.frontend_theme.main')
 @section('mainContent')
 <section>
-<div class="container">
-<div class="row">
-<div class="col-sm-8 order-sm-0 order-2">
-    <div class="mb-sm-3">
-<h1>{{ $pageTitle??'' }}</h1>
-<p class="mt-4">This page is currently under construction. </p>
-</div>
-</div>
-</div>
-</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="project-card-wrapper">
+                    <a href="#" class="text-decoration-none">
+                    <div class="project-image-wrapper h-100">
+                        <img src="{{ URL::to('assets/website/img/project.jpg') }}" alt="Project Title" class="img-fluid project-image h-100">
+                    </div>
+                    <div class="project-details-wrapper h-100 p-3">
+                        <div>
+                            <h5 class="mb-0">Assetz Marq 2</h5>
+                        </div>
+                        <p><i class="fa-solid fa-location-dot"></i> Kannamangala, Bangalore</p>
+                        <p>3 BHK Apartment 1366 - 1600 sqft</p>
+                        <p class="mb-0"> <span class="price-info">₹90.0 L*</span> <small>Onwards</small></p>
+                    </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 @endsection
