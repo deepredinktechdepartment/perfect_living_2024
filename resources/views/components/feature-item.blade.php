b01d30b7e5702bcb23233e5b75f8e0381a334127
deepredinktechdepartment <109503655+deepredinktechdepartment@users.noreply.github.com>
2024-09-20 18:27:36 +0530

    Update feature-item.blade.php

--- resources/views/components/feature-item.blade.php
+++ resources/views/components/feature-item.blade.php
@@ -1,8 +1,8 @@
 <!-- resources/views/components/feature-item.blade.php -->
 
 <div class="feature-badges-wrapper">
-    <div class="d-flex gap-3 align-items-center mb-3">
+    <div class="d-flex gap-2 align-items-center mb-3">
         <img src="{{ $imageSrc }}" alt="{{ $altText }}" class="img-fluid">
-  <h6>{{ $title }}</h6>
+  <h6 class="mb-0">{{ $title }}</h6>
     </div>
 </div>
